b153576618dfc040c9f5b64c4047b6f7451a3d0a
Paolo Lami <paololami2000@gmail.com>
2021-10-31 02:29:49 +0000

    sens test

--- senstest.py
+++ senstest.py
@@ -1,6 +1,27 @@
-import PCF8591 as ADC
+import smbus
+import time
 
-sens = ADC(0x48)
+class PCF8591:
+
+  def __init__(self,address):
+    self.bus = smbus.SMBus(1)
+    self.address = address
+
+  def read(self,chn): #channel
+      try:
+          self.bus.write_byte(self.address, 0x40 | chn)  # 01000000
+          self.bus.read_byte(self.address) # dummy read to start conversion
+      except Exception as e:
+          print ("Address: %s \n%s" % (self.address,e))
+      return self.bus.read_byte(self.address)
+
+  def write(self,val):
+      try:
+          self.bus.write_byte_data(self.address, 0x40, int(val))
+      except Exception as e:
+          print ("Error: Device address: 0x%2X \n%s" % (self.address,e))
+
+sens = PCF8591(0x48)
 
 def loop():
 	while True:
